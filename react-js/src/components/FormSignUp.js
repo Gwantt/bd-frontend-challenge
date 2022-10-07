8ed3f45d0586d5665f8a0877c8c50ea784dc2c3e
grant <grntwalton@gmail.com>
2022-10-07 12:25:58 -0700

    basic setup complete working on first step of form

---
+++ react-js/src/components/FormSignUp.js
@@ -0,0 +1,18 @@
+import React, {useState} from "react"
+
+
+const FormSignUp = () => {
+
+    const [step, setStep] = useState(0)
+    
+
+    return (
+        <div>
+            <div className="progressbar">
+                <div className="">
+
+                </div>
+            </div>
+        </div>
+    )
+}
